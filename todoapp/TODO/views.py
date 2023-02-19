166ea4e3549cb8a1e155e6228163147b5f13270b
Posvian <posvianski@mail.ru>
2023-02-19 23:55:45 +0100

    made task_4

--- todoapp/TODO/views.py
+++ todoapp/TODO/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
+from .filters import ProjectFilter, TODOFilter
 from .models import Project, TODO
 from .serializers import ProjectModelSerializer, TODOModelSerializer
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 
 
-class ProjectModelViewSet(ModelViewSet):
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class TODOLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
+
+
+class ProjectCustomDjangoFilterViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
 
 
-class TODOModelViewSet(ModelViewSet):
+class TODODjangoFilterModelViewSet(ModelViewSet):
     queryset = TODO.objects.all()
     serializer_class = TODOModelSerializer
+    pagination_class = TODOLimitOffsetPagination
+    filterset_class = TODOFilter
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.active = False
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def perform_destroy(self, instance):
+        pass
--- todoapp/todoapp/settings.py
+++ todoapp/todoapp/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'users',
     'TODO',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,13 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
