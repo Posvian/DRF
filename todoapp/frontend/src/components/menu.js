86b746416f38e145ab8a552bbf5b4c042feeb025
Posvian <posvianski@mail.ru>
2023-03-06 18:33:59 +0100

    upgrade

--- todoapp/frontend/src/components/menu.js
+++ todoapp/frontend/src/components/menu.js
@@ -1,23 +1,25 @@
 import React from "react";
 import {Link, BrowserRouter} from "react-router-dom";
-import App from "../App";
+
 
 
 const Menu = () => {
     return (
         <BrowserRouter>
-          <ul className="menu">
-            <li>
-              <Link to='/'> Users</Link>
-            </li>
-            <li>
-              <Link to='/projects'> Projects</Link>
-            </li>
-            <li>
-              <Link to='/todo'> Todo </Link>
-            </li>
-            {/*<li><a href="#">О нас</a></li>*/}
-          </ul>
+          <nav>
+            <ul className="menu">
+              <li>
+                <Link to='/'> Users</Link>
+              </li>
+              <li>
+                <Link to='/projects'> Projects</Link>
+              </li>
+              <li>
+                <Link to='/todo'> Todo </Link>
+              </li>
+              {/*<li><a href="#">О нас</a></li>*/}
+            </ul>
+          </nav>
         </BrowserRouter>
     )
 }
